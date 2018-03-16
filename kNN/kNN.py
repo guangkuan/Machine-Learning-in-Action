d19e1132fa3d038e3e3a698475e69694214128e8
guangkuan <32082897+guangkuan@users.noreply.github.com>
2018-03-16 17:13:42 +0800

    jj

--- kNN/kNN.py
+++ kNN/kNN.py
@@ -9,7 +9,7 @@ from numpy import *
 import operator
 from os import listdir
 
-def createDataSet():
+#def createDataSet():
     group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
     labels = ['A', 'A', 'B', 'B']
     return group, labels
